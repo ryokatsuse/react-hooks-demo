d76e6bb14ce5c2e72ab76d19fa74930be6b70651
ryokatsuse <ryokatsu0719@gmail.com>
2020-04-27 17:57:43 +0900

    feat:複数の状態を管理する

--- src/App.js
+++ src/App.js
@@ -1,42 +1,29 @@
 import React, { useState } from 'react';
 
-const App = () => {
-  const [count, setCount] = useState(0)
+const App = props => {
 
-  const increment = () => setCount(count + 1)
-  const decrement = () => setCount(count - 1)
+  const [name, setName] = useState(props.name)
+  const [price, setPrice] = useState(props.price)
 
-  const increment2 = () => setCount(previousCount => previousCount + 1)
-  const decrement2 = () => setCount(previousCount => previousCount - 1)
-
-  const resetCount = () => setCount(0)
-  const multipleCount = () => setCount(previousCount => previousCount * 2)
-  const threeCount = () => setCount(previousCount => {
-    if(previousCount % 3 === 0){
-      return previousCount / 3
-    } else {
-      return previousCount
-    }
-  })
+  const reset = () => {
+    setPrice(props.price)
+    setName(props.name)
+  }
 
   return (
     <>
-      <div>count: {count}</div>
-      <div>
-        <button onClick={increment}>+</button>
-        <button onClick={decrement}>-</button>
-      </div>
-      <div>
-        <button onClick={increment2}>+</button>
-        <button onClick={decrement2}>-</button>
-      </div>
-      <div>
-        <button onClick={resetCount}>Reset</button>
-        <button onClick={multipleCount}>x2</button>
-        <button onClick={threeCount}>3の倍数のときだけ押すと割る</button>
-      </div>
+      <p>現在の{name}は、{price}円です。</p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>reset</button>
+      <input value={name} onChange={e => setName(e.target.value)} />
     </>
   );
+}
+
+App.defaultProps = {
+  name: '',
+  price: 1000
 }
 
 export default App;
